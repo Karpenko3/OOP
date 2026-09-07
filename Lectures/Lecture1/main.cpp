ece632491f3a362ecd0bb6fe94058eccb9abcc53
Артем <akarpenko134@gmail.com>
2026-09-07 16:41:27 +0300

    Creates repo for KhPI labs OOP

---
+++ Lectures/Lecture1/main.cpp
@@ -0,0 +1,11 @@
+#include <header.h>
+#include <source.cpp>
+
+int main(){
+
+class Myst ob1, ob2;
+ob1.salery();
+ob2.show();
+
+
+} 
---
+++ Lectures/Lecture1/source.cpp
@@ -0,0 +1,20 @@
+#include <header.h>
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+ void Myst::salery(){
+    cin >> x;
+    cin >> y;
+   };
+   
+   void Myst::show(){
+    cout << x << y;
+   }
+
+ void Myst::set_xy( int x, int y){
+ 
+  this -> x = x;
+  this -> y = y;
+
+ }
